737e23ed2136306c56f663c84a46cc1f966ff714
Ahmad Qureshi <ahqu00001@stud.uni-saarland.de>
2023-05-19 17:26:59 +0200

    HELLLLLLO CAN YOU H3EAR ME

--- src/image_edit.c
+++ src/image_edit.c
@@ -86,8 +86,6 @@ void mirror_vertical(image_t *img) {
         for (int j = 0; j< width; j++) 
         {
             //we want the pixel from our pixel array, and we have the formula 
-            /*pixel_t* srcpixel = &(img->img[i * width + j]);*/
-            /*pixel_t temp = img->pixels[y * width + x];*/
             int rotX=j;
             int rotY=height-1-i;
 
@@ -100,9 +98,39 @@ void mirror_vertical(image_t *img) {
     img->img=new_image.img;
 }
 
-void resize(image_t *img, int new_width, int new_height) {
-    NOT_IMPLEMENTED;
-    UNUSED(img);
-    UNUSED(new_width);
-    UNUSED(new_height);
-}
+void resize(image_t *img, int new_width, int new_height) 
+{
+
+    image_t new_image;
+    int width=img->w;
+    int height=img->h;
+    //CASE 1:
+    //CASE 2 to reduce size
+    new_image.img= malloc(new_width * new_height * sizeof(pixel_t)); //NEW ARRAY
+    int i, j;
+    //BLACK IS INPUTTED IN WHOLE NEW BUFFER
+    for (i = 0; i < new_height*new_width; i++) 
+    {
+        new_image.img[i].r = 0;
+        new_image.img[i].g = 0;
+        new_image.img[i].b= 0;
+    }
+    int copy_width = (new_width< img->w) ? new_width : img->w;
+    int copy_height = (new_height < img->h) ? new_height : img->h;
+    ///LOOP WHICH COPIES OLD IMAGE TO NEW
+    for (int i = 0; i <copy_height; i++) 
+    {
+        for (int j = 0; j<copy_width; j++) 
+        {         
+        //We use code from vertical but get rid of the rotX/Y because we don't rotate just resize
+        //Important note here is that two cases or maybe 4 cases        
+        new_image.img[i*new_width+j] = img->img[i*img->w+j];            
+        }
+    }
+    
+    free(img->img); //code passed two tests so basically we have resized to a new 
+    img->w=new_width;
+    img->h=new_height;
+    img->img=new_image.img;
+
+} 
