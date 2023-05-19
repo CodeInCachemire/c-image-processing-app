781abb5d0d066049bc88eb7de30621d55d6a29ee
Ahmad Qureshi <ahqu00001@stud.uni-saarland.de>
2023-05-19 05:11:03 +0200

    hallo from the brother side

--- src/image_edit.c
+++ src/image_edit.c
@@ -6,11 +6,6 @@
 #include "util.h"
 
 void rotate_counterclockwise(image_t *img) {
-
-}
-
-void rotate_clockwise(image_t *img) {
-    /////CASEEEEE1;
     image_t new_image;
     int width=img->h;
     int height=img->w;
@@ -20,7 +15,31 @@ void rotate_clockwise(image_t *img) {
         for (int j = 0; j< width; j++) 
         {
             //we want the pixel from our pixel array, and we have the formula 
-            pixel_t* srcpixel = &(img->img[i * width + j]);
+            /*pixel_t* srcpixel = &(img->img[i * width + j]);*/
+            int rotX=i;
+            int rotY=height-1-j;
+
+            new_image.img[rotY*width+rotX] = img->img[i*img->w+j];
+        }
+    }
+    free(img->img);
+    img->w=width;
+    img->h=height;
+    img->img=new_image.img;
+
+}
+
+void rotate_clockwise(image_t *img) {
+    image_t new_image;
+    int width=img->h;
+    int height=img->w;
+    new_image.img= malloc(width * height * sizeof(pixel_t)); //NEW ARRAY
+    for (int i = 0; i <height; i++) 
+    {
+        for (int j = 0; j< width; j++) 
+        {
+            //we want the pixel from our pixel array, and we have the formula 
+            /*pixel_t* srcpixel = &(img->img[i * width + j]);*/
             int rotX=height-1-i;
             int rotY=j;
 
@@ -35,12 +54,50 @@ void rotate_clockwise(image_t *img) {
 }
 
 void mirror_horizontal(image_t *img) {
+    image_t new_image;
+    int width=img->w;
+    int height=img->h;
+    new_image.img= malloc(width * height * sizeof(pixel_t)); //NEW ARRAY
+    for (int i = 0; i <height; i++) 
+    {
+        for (int j = 0; j< width; j++) 
+        {
+            //we want the pixel from our pixel array, and we have the formula 
+            /*pixel_t* srcpixel = &(img->img[i * width + j]);*/
+            int rotX=height-1-j;
+            int rotY=i;
 
+            new_image.img[rotY*width+rotX] = img->img[i*img->w+j];
+        }
+    }
+    free(img->img);
+    img->w=width;
+    img->h=height;
+    img->img=new_image.img;
 }
 
 void mirror_vertical(image_t *img) {
-    NOT_IMPLEMENTED;
-    UNUSED(img);
+    image_t new_image;
+    int width=img->w;
+    int height=img->h;
+    new_image.img= malloc(width * height * sizeof(pixel_t)); //NEW ARRAY
+    for (int i = 0; i <height; i++) 
+    {
+        for (int j = 0; j< width; j++) 
+        {
+            //we want the pixel from our pixel array, and we have the formula 
+            /*pixel_t* srcpixel = &(img->img[i * width + j]);*/
+            /*pixel_t temp = img->pixels[y * width + x];*/
+            int rotX=j;
+            int rotY=height-1-i;
+
+            new_image.img[rotY*width+rotX] = img->img[i*img->w+j];
+        }
+    }
+    free(img->img);
+    img->w=width;
+    img->h=height;
+    img->img=new_image.img;
 }
 
 void resize(image_t *img, int new_width, int new_height) {
